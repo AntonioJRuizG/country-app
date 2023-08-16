ffc1e4739d7fe6e96c2344d0118d281c651fe015
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-16 05:45:13 +0200

    Add countries pages

---
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'countries-by-capital-page',
+  templateUrl: './by-capital-page.component.html',
+})
+export class ByCapitalPageComponent {}
