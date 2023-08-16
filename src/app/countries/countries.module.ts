5c014ff17d6e7725f7520449b4488be406e3cbe8
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-16 21:14:04 +0200

    Add countries gallery

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -6,6 +6,7 @@ import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.com
 import { CountryPageComponent } from './pages/country-page/country-page.component';
 import { CountriesRoutingModule } from './countries-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { CountryGalleryComponent } from './components/country-gallery/country-gallery.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SharedModule } from '../shared/shared.module';
     ByCountryPageComponent,
     ByRegionPageComponent,
     CountryPageComponent,
+    CountryGalleryComponent,
   ],
   imports: [CommonModule, CountriesRoutingModule, SharedModule],
 })
