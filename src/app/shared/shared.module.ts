611192ed283e6bc904ab88f5215943e209ea6dce
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-16 05:15:33 +0200

    Add contact page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,10 @@ import { AboutComponent } from './pages/about/about.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
-  declarations: [HomePageComponent, AboutComponent, SidebarComponent],
+  declarations: [HomePageComponent, AboutComponent, SidebarComponent, ContactComponent],
   imports: [CommonModule, RouterModule],
   exports: [HomePageComponent, AboutComponent, SidebarComponent],
 })
