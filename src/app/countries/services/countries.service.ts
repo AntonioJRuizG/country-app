a66ed47660b35e55d283c8a464536be6bf63c46f
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-16 23:51:53 +0200

    Add get country info service

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Country } from '../interfaces/country';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
@@ -22,5 +22,14 @@ export class CountriesService {
   searchCountryByRegion(searchValue: string): Observable<Country[]> {
     const url = `${this.countriesApiUri}/region/${searchValue}`;
     return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+  }
+
+  searchCountryByFullName(searchValue: string): Observable<Country | null> {
+    const url = `${this.countriesApiUri}/name/${searchValue}?fullText=true`;
+
+    return this.http.get<Country[]>(url).pipe(
+      map((countries) => (countries.length > 0 ? countries[0] : null)),
+      catchError(() => of(null))
+    );
   }
 }
