a3b77b0bf38c1a65b1c8ad88302422bb3a64ca24
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-17 10:13:08 +0200

    Refactor http requests

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -9,19 +9,23 @@ export class CountriesService {
 
   constructor(private http: HttpClient) {}
 
+  private getCountriesRequest(url: string): Observable<Country[]> {
+    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+  }
+
   searchCountryByCapital(searchValue: string): Observable<Country[]> {
     const url = `${this.countriesApiUri}/capital/${searchValue}`;
-    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+    return this.getCountriesRequest(url);
   }
 
   searchCountryByName(searchValue: string): Observable<Country[]> {
     const url = `${this.countriesApiUri}/name/${searchValue}`;
-    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+    return this.getCountriesRequest(url);
   }
 
   searchCountryByRegion(searchValue: string): Observable<Country[]> {
     const url = `${this.countriesApiUri}/region/${searchValue}`;
-    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+    return this.getCountriesRequest(url);
   }
 
   searchCountryByCode(searchValue: string): Observable<Country | null> {
