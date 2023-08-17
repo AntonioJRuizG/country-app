0909db9b6183bd3029e66bd985fcb2423920d4bb
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-17 15:30:27 +0200

    Improve search by region

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Americas' | 'Africa' | 'Asia' | 'Europe' | 'Oceania';
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -9,11 +10,21 @@ import { Country } from '../../interfaces/country';
 })
 export class ByRegionPageComponent {
   public countriesList: Country[] = [];
+  public regions: Region[] = [
+    'Americas',
+    'Africa',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
   public isLoading: boolean = false;
   public voidSearchTerm: boolean = true;
+  public selectedRegion?: string;
+
   constructor(private searchService: CountriesService) {}
 
-  searchByRegion(term: string) {
+  searchByRegion(term: Region) {
+    this.selectedRegion = term;
     term ? (this.voidSearchTerm = false) : (this.voidSearchTerm = true);
     this.isLoading = true;
     this.searchService.searchCountryByRegion(term).subscribe((countries) => {
